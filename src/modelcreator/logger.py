66709b9dd4c2edca80b22b3d6122d40fd9ed8059
ML195 <88842385+ML195@users.noreply.github.com>
2021-08-13 09:42:49 +0200

    Added a mkdir if the log folder does not exist

--- src/modelcreator/logger.py
+++ src/modelcreator/logger.py
@@ -5,7 +5,9 @@ from pathlib import Path
 def setup_info_logger():
     formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(module)s - %(message)s')
 
-    log_file_path = Path(__file__).parents[1].resolve() / 'logs' / 'info.log'
+    log_dir_path = Path(__file__).parents[1].resolve() / 'logs'
+    log_dir_path.mkdir(mode=0o777, parents=True, exist_ok=True)
+    log_file_path = log_dir_path / 'info.log'
 
     file_handler = logging.FileHandler(log_file_path)
     file_handler.setFormatter(formatter)
